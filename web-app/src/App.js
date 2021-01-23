5d693bb204e754f7c419c49c4b415accaf59adbf
“jeremy-brody” <jeremy.brody@gmail.com>
2021-01-23 10:45:46 -0500

    swipe

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import MqttTile from './MqttTile';
 
 function App() {
   return (
     <div className="App">
-        <MqttTile mqttTopic={'home/livingroom/temperature'} />
+      <Swiper> 
+        <SwiperSlide><MqttTile mqttTopic={'home/livingroom/temperature'} /></SwiperSlide>
+        <SwiperSlide><MqttTile mqttTopic={'home/livingroom/humidity'} /></SwiperSlide>
+      </Swiper>
     </div>
   );
 }
