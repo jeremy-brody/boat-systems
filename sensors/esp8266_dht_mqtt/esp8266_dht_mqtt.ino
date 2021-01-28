ce340a2baae9772f2f921ecf8c20c490a242174e
“jeremy-brody” <jeremy.brody@gmail.com>
2021-01-28 09:08:16 -0500

    dht sensor refactor

--- sensors/esp8266_dht_mqtt/esp8266_dht_mqtt.ino
+++ sensors/esp8266_dht_mqtt/esp8266_dht_mqtt.ino
@@ -4,20 +4,26 @@
 #include <ArduinoJson.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <ArduinoOTA.h>
 
+const int interval = 10000; // ms
 const char* ssid = "Chewbacca";
 const char* password = "pv2ohf2j1[pjgrf";
 const char* mqtt_server = "192.168.7.79";
-//const String clientId = "ESP8266-001";
-//const String description = "Living Room";
-//const char* mqtt_topic_temperature = "home/livingroom/temperature";
-//const char* mqtt_topic_humidity = "home/livingroom/humidity";
-const String clientId = "ESP8266-002";
-const String description = "Basement";
-const char* mqtt_topic_temperature = "home/basement/temperature";
-const char* mqtt_topic_humidity = "home/basement/humidity";
 const String sensor_type_humidity = "Humidity";
 const String sensor_type_temperature = "Temperature";
+const String temperature_unit = "F";
+const String humidity_unit = "%";
+
+const String clientId = "ESP8266-001";
+const String description = "Living Room";
+const char* mqtt_topic_temperature = "home/livingroom/temperature";
+const char* mqtt_topic_humidity = "home/livingroom/humidity";
+
+//const String clientId = "ESP8266-002";
+//const String description = "Basement";
+//const char* mqtt_topic_temperature = "home/basement/temperature";
+//const char* mqtt_topic_humidity = "home/basement/humidity";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -28,46 +34,49 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-void setup_wifi() {
+void begin_dht() {
+  dht.begin();
+  Serial.println("[ OK ] DHT");
+}
 
+void begin_ntp() {
+  timeClient.begin();
+  Serial.println("[ OK ] NTP");
+}
+
+void connect_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-
   randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("[ OK ] WiFi, IP: ");
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() {
-  // Loop until we're reconnected
+void connect_mqtt() {
+  client.setServer(mqtt_server, 1883);
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      publish();
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
+    if (!client.connect(clientId.c_str())) {
       delay(5000);
     }
   }
+  Serial.println("[ OK ] MQTT");
+}
+
+char* buildMessage(String sensor, long epochTime, String unit, float value) {
+      StaticJsonDocument<256> doc;
+      doc["sensor"] = sensor;
+      doc["time"] = epochTime;
+      doc["unit"] = unit;
+      doc["value"] = value;
+      doc["description"] = description;
+      char output[256];
+      serializeJson(doc, output);
+      Serial.println(output);
+      return output;
 }
 
 void publish() {
@@ -75,43 +84,28 @@ void publish() {
       long epochTime = timeClient.getEpochTime();
       float humidity = dht.readHumidity();
       float temperature = dht.readTemperature(true); // true == farenheit
-      client.publish(mqtt_topic_temperature, buildMessage(sensor_type_temperature, epochTime, "F", temperature));
-      client.publish(mqtt_topic_humidity, buildMessage(sensor_type_humidity, epochTime, "%", humidity));
-}
-
-char* buildMessage(String sensor, long epochTime, String unit, float value) {
-      StaticJsonDocument<256> doc;
-
-      doc["sensor"] = sensor;
-      doc["time"] = epochTime;
-      doc["unit"] = unit;
-      doc["value"] = value;
-      doc["description"] = description;
-      
-      char output[256];
-      serializeJson(doc, output);
-
-      Serial.println(output);
-      return output;
+      client.publish(mqtt_topic_temperature, buildMessage(sensor_type_temperature, epochTime, temperature_unit, temperature));
+      client.publish(mqtt_topic_humidity, buildMessage(sensor_type_humidity, epochTime, humidity_unit, humidity));
 }
 
 void setup() {
   Serial.begin(115200);
-  dht.begin();
-  timeClient.begin();
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  Serial.println("\r\n\r\nBooting");
+  begin_dht();
+  begin_ntp();
+  connect_wifi();
+  connect_mqtt();
+  Serial.println("Boot Complete\r\n");
 }
 
 void loop() {
-
   if (!client.connected()) {
-    reconnect();
+    connect_mqtt();
   }
   client.loop();
 
   unsigned long now = millis();
-  if (now - lastMsg > 10000) {
+  if (now - lastMsg > interval) {
     lastMsg = now;
     publish();
   }
