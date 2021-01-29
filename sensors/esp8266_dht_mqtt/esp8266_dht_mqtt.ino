76ec17b131feb4fbb140c54f569db9cc6c463779
“jeremy-brody” <jeremy.brody@gmail.com>
2021-01-28 21:45:53 -0500

    add microcontroller ota updates

--- sensors/esp8266_dht_mqtt/esp8266_dht_mqtt.ino
+++ sensors/esp8266_dht_mqtt/esp8266_dht_mqtt.ino
@@ -44,6 +44,37 @@ void begin_ntp() {
   Serial.println("[ OK ] NTP");
 }
 
+void begin_ota() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("[ OK ] OTA");
+}
+
 void connect_wifi() {
   delay(10);
   WiFi.mode(WIFI_STA);
@@ -93,12 +124,15 @@ void setup() {
   Serial.println("\r\n\r\nBooting");
   begin_dht();
   begin_ntp();
+  begin_ota();
   connect_wifi();
   connect_mqtt();
   Serial.println("Boot Complete\r\n");
 }
 
 void loop() {
+  ArduinoOTA.handle();
+  
   if (!client.connected()) {
     connect_mqtt();
   }
